fc7a981e91e3b66539c28d52fa205a91a5b6afec
Ben Hardill <ben@flowforge.com>
2026-08-12 14:02:11 +0100

    Add kill switch env var
    fixes #180

--- nodes/ff-mqtt.js
+++ nodes/ff-mqtt.js
@@ -52,6 +52,10 @@ module.exports = function (RED) {
         throw new Error('FlowFuse MQTT nodes cannot be loaded due to missing instance information')
     }
 
+    if (process.env['FFNODE_DISABLE_MQTT'] && process.env['FFNODE_DISABLE_MQTT'] === 'true') {
+		return
+	}
+
     const TEAM_CLIENT_AUTH_ID = createNrMqttId(forgeSettings.teamID, instanceType, instanceId) // e.g., 'mq:remote:teamId:deviceId' or 'mq:hosted:teamId:projectId:haId'
     const TEAM_CLIENT_CLIENT_ID = createNrMqttClientId(forgeSettings.teamID, instanceType, instanceId, haInstanceId) // e.g., 'mq:remote:deviceId' or 'mq:hosted:projectId:haId'
     const ALLOW_DYNAMIC_CONNECT_OPTIONS = false // disable dynamic connect options for this type of node (fixed broker connection)
